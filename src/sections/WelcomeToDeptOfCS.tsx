4570ab4ababc8ad5ba5ab27b4794d4e2ab668442
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-08-08 02:54:36 +0530

    + Section: 2-WelcomeToDeptOfCS ✨🖐️

---
+++ src/sections/WelcomeToDeptOfCS.tsx
@@ -0,0 +1,20 @@
+import gifs from "../assets/gifs";
+
+function WelcomeToDeptOfCS() {
+  return (
+    <section className="flex-center flex-col overflow-clip h-screen w-screen">
+      <div className="">
+        <img className="h-64 w-auto" src={getGif(gifs.welcome)} alt="" />
+      </div>
+      <br />
+      <h1 className="font-fascinate">To the Department of</h1>
+      <br />
+      <div className="flex flex-row relative">
+        <img className="h-64 w-auto" src={getGif(gifs.computer)} alt="" />
+        <img className="h-64 w-auto" src={getGif(gifs.science)} alt="" />
+      </div>
+    </section>
+  );
+}
+
+export default WelcomeToDeptOfCS;
