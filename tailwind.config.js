52a2d902cc71354efe9d09050cb192a92137cc28
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-08-07 22:00:57 +0530

    + tailwind support 🔧🔨

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fascinate: ["Fascinate", "cursive"],
+        "porter-sans": ["porter-sans-inline-block"],
+      },
+    },
+  },
+  plugins: [],
+};
