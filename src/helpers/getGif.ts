02c9361b47ef6ab8765a99eb00867f9410a4b00f
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-08-08 02:55:45 +0530

    + Helpers: getGif() 🔨🔧

---
+++ src/helpers/getGif.ts
@@ -0,0 +1,16 @@
+type gif = {
+  path?: string;
+  url: string;
+  alt?: string;
+};
+
+function getGif(gif: gif) {
+  try {
+    if ("path" in gif) {
+      return gif.path;
+    }
+    return gif.url;
+  } catch (error: any) {
+    console.log("Error on geting GIF link:", error);
+  }
+}
