3a8e2d59f99c9654a935403a8a46633a7828e457
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-08-08 02:51:36 +0530

    + assets links ✨✨

---
+++ src/assets/gifs/index.ts
@@ -0,0 +1,55 @@
+import Hi_DJ_Khaled from "./Hi_DJ_Khaled.gif";
+import Hi_Friends_Rachel from "./Hi_Friends_Rachel.gif";
+import Welcome from "./Welcome.gif";
+import Computer from "./Computer.gif";
+import Science from "./Science.gif";
+import CS_Learn from "./CS_Learn.gif";
+import Binary_Captain_Marvel from "./Binary_Captain_Marvel.gif";
+import Binary from "./Binary.gif";
+import Code from "./Code.gif";
+import Breaks_Computer_Panda from "./Breaks_Computer_Panda.gif";
+
+const gifs: Record<string, gif> = {
+  hi_dj_khaled: {
+    path: Hi_DJ_Khaled,
+    url: "https://media.giphy.com/media/QBjok2NBIzSR7IaDQK/giphy.gif",
+  },
+  hi_friends_rachel: {
+    path: Hi_Friends_Rachel,
+    url: "https://media.giphy.com/media/XGhTPVMgzLv7s2TOE6/giphy.gif",
+  },
+  welcome: {
+    path: Welcome,
+    url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHdibDc0djh2cnZ1NXNpbmVuZDhmcHhlamw5N3ZobjAyODVqZzN6ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUPGGDNsLvqsBOhuU0/giphy.gif",
+  },
+  computer: {
+    path: Computer,
+    url: "https://media.giphy.com/media/Nhw1FwkchB2cIozD12/giphy.gif",
+  },
+  science: {
+    path: Science,
+    url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHp4dXEyMnA5YWd4ZGIzNmdzOTR6M3JtZ3A4cWg4NTdyb3JocndzMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fqIBaMWI7m7O8/giphy.gif",
+  },
+  cs_learn: {
+    path: CS_Learn,
+    url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGNobzc2N3MwbWhpdDJoMHh6dDh5OG9ncm93MG1wcHNoc202YWdoOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WemtkIRyd2JMciY3Zh/giphy.gif",
+  },
+  binary_captain_marvel: {
+    path: Binary_Captain_Marvel,
+    url: "https://media.giphy.com/media/4ac7L7AgH6pyJPwsQO/giphy.gif",
+  },
+  binary: {
+    path: Binary,
+    url: "https://media.giphy.com/media/AHcEGB5nuIALBqKWjp/giphy.gif",
+  },
+  code: {
+    path: Code,
+    url: "https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif",
+  },
+  breaks_computer_panda: {
+    path: Breaks_Computer_Panda,
+    url: "https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif",
+  },
+};
+
+export default gifs;
