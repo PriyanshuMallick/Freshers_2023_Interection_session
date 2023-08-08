0e7603dd5d532ea979486b1e249678223dc9549d
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-08-09 02:07:59 +0530

    + Game page and a button to go to that page 🎮🧩✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
+import GameButton from "./components/GameButton";
 import HelloFreshers from "./sections/HelloFreshers";
 import WelcomeToDeptOfCS from "./sections/WelcomeToDeptOfCS";
 import WhereYouWillLearn from "./sections/WhereYouWIllLearn";
+import GamePage from "./sections/GamePage";
 
 function App() {
+  const [isGameMode, setIsGameMode] = useState(false);
+
   return (
-    <div>
-      <HelloFreshers />
-      <WelcomeToDeptOfCS />
-      <WhereYouWillLearn />
-    </div>
+    <>
+      {!isGameMode && (
+        <div>
+          <HelloFreshers />
+          <WelcomeToDeptOfCS />
+          <WhereYouWillLearn />
+          <div className="h-screen w-screen flex-center">
+            <div>
+              <GameButton
+                text="Start Game"
+                onClick={() => setIsGameMode(true)}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+
+      {isGameMode && (
+        <div>
+          <GamePage />
+        </div>
+      )}
+    </>
   );
 }
 
