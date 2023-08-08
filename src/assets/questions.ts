3557e4ab7ac924d07052d2b92ffe28cb5f8c0a8b
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-08-09 02:05:19 +0530

    + Questions ⁉✨

---
+++ src/assets/questions.ts
@@ -0,0 +1,70 @@
+import gameGifs from "./gifs/game";
+
+const questions = [
+  {
+    q: "Do a Dumb Sharas",
+    gif: [gameGifs.dumbsharas1, gameGifs.dumbsharas2],
+  },
+  {
+    q: "Do a Dumb Sharas",
+    gif: [gameGifs.dumbsharas1, gameGifs.dumbsharas2],
+  },
+  {
+    q: "Do a Dumb Sharas",
+    gif: [gameGifs.dumbsharas1, gameGifs.dumbsharas2],
+  },
+  {
+    q: "Do a Dumb Sharas",
+    gif: [gameGifs.dumbsharas1, gameGifs.dumbsharas2],
+  },
+  {
+    q: "Do a Dumb Sharas",
+    gif: [gameGifs.dumbsharas1, gameGifs.dumbsharas2],
+  },
+  {
+    q: "Do a Dumb Sharas",
+    gif: [gameGifs.dumbsharas1, gameGifs.dumbsharas2],
+  },
+  {
+    q: "Do a Dumb Sharas",
+    gif: [gameGifs.dumbsharas1, gameGifs.dumbsharas2],
+  },
+  {
+    q: "Sing A Song",
+    gif: [gameGifs.sing1, gameGifs.sing2],
+  },
+  {
+    q: "Recite a poem in a funny voice",
+    gif: [gameGifs.reading1],
+  },
+  {
+    q: "Show your talent",
+    gif: [gameGifs.talent1],
+  },
+  {
+    q: "Do a dance",
+    gif: [gameGifs.dance1],
+  },
+  {
+    q: "Make Every Body Laugh",
+    gif: [gameGifs.laugh1, gameGifs.laugh1],
+  },
+  {
+    q: "Eat a snack without using your hand",
+    gif: [gameGifs.eat1],
+  },
+  {
+    q: "Introduce Yourself acting like a robot",
+    gif: [gameGifs.robot1, gameGifs.robot2],
+  },
+  {
+    q: "Tell a funny story or a Joke in funny voice",
+    gif: [gameGifs.laugh1, gameGifs.laugh1],
+  },
+  // {
+  //   q: "Propose an object",
+  //   gif: [gameGifs.propose1],
+  // },
+];
+
+export default questions;
