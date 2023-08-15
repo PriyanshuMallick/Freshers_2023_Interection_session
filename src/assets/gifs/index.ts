dfe34c6cda0dade0e8b175120cbdddd75bf53fc5
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-08-15 11:57:36 +0530

    + Alternate Text

--- src/assets/gifs/index.ts
+++ src/assets/gifs/index.ts
@@ -3,33 +3,43 @@ import { gif } from "../../helpers/getGif";
 const gifs: Record<string, gif> = {
   hi_dj_khaled: {
     url: "https://media.giphy.com/media/QBjok2NBIzSR7IaDQK/giphy.gif",
+    alt: "DK Khaled waving hi GIF",
   },
   hi_friends_rachel: {
     url: "https://media.giphy.com/media/XGhTPVMgzLv7s2TOE6/giphy.gif",
+    alt: "Rachel from the TV show Friends waving hi GIF",
   },
   welcome: {
     url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHdibDc0djh2cnZ1NXNpbmVuZDhmcHhlamw5N3ZobjAyODVqZzN6ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUPGGDNsLvqsBOhuU0/giphy.gif",
+    alt: "Welcome Text GIF",
   },
   computer: {
     url: "https://media.giphy.com/media/Nhw1FwkchB2cIozD12/giphy.gif",
+    alt: "GIF of a computer",
   },
   science: {
     url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHp4dXEyMnA5YWd4ZGIzNmdzOTR6M3JtZ3A4cWg4NTdyb3JocndzMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fqIBaMWI7m7O8/giphy.gif",
+    alt: "A guy shouting 'Science' GIF",
   },
   cs_learn: {
     url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGNobzc2N3MwbWhpdDJoMHh6dDh5OG9ncm93MG1wcHNoc202YWdoOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WemtkIRyd2JMciY3Zh/giphy.gif",
+    alt: "A animated kid showing the text strings, number, boolean GIF",
   },
   binary_captain_marvel: {
     url: "https://media.giphy.com/media/4ac7L7AgH6pyJPwsQO/giphy.gif",
+    alt: "Scene from Captain Marvel Movie showing Captain Marvel going binary and leaving earth GIF",
   },
   binary: {
     url: "https://media.giphy.com/media/AHcEGB5nuIALBqKWjp/giphy.gif",
+    alt: "A Gif showing binary 1's and 0's",
   },
   code: {
     url: "https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif",
+    alt: "GIF showing some code",
   },
   breaks_computer_panda: {
     url: "https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif",
+    alt: "GIF of a panda breaking a computer in an office out of frustration",
   },
 };
 
