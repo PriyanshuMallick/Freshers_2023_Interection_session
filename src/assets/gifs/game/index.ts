cb4de6cb33ba8a26c0e3abd02120d72633ef0d5c
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-08-15 11:46:59 +0530

    - Removed Local GIF paths

--- src/assets/gifs/game/index.ts
+++ src/assets/gifs/game/index.ts
@@ -1,99 +1,72 @@
-import dumbsharas_ross_friends from "./dumbsharas_ross_friends.gif";
-import dumbsharas2 from "./dumbsharas2.gif";
-import sing1 from "./sing_top_gun.gif";
-import sing2 from "./Animated_Viking_women_singing.gif";
-import dance1 from "./Jimmy_Fallon_Dancing.gif";
-import dance2 from "./dance_wizard.gif";
-import reading1 from "./Poem_Beauty_and_the_Beast.gif";
-import talent1 from "./Bugs_Bunny_jumping_on_Piano.gif";
-import laugh1 from "./mr_bean_showing_tongue.gif";
-import laugh2 from "./mr_bean_goof.gif";
-import eat1 from "./patrick_eats_food.gif";
-import robot1 from "./robot_dance.gif";
-import robot2 from "./robot_army.gif";
-import propose1 from "./tom_propose.gif";
+import { gif } from "../../../helpers/getGif";
 
-const gameGifs = {
+const gameGifs: Record<string, gif> = {
   dumbsharas1: {
-    path: dumbsharas_ross_friends,
     url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHNhOWs1cjdxOWtvcHM3YnZwMW83MHY0cm5ueW9mb3plem15bndzMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lngHw0XWSu8tsNZRfU/giphy-downsized-large.gif",
     alt: "Dumb Sharas and Ross Friends",
   },
 
   dumbsharas2: {
-    path: dumbsharas2,
     url: "https://media.giphy.com/media/2jCg3WIQKJUmwb7RuE/giphy.gif",
     alt: "Dumb Sharas GIF",
   },
 
   sing1: {
-    path: sing1,
     url: "https://media.giphy.com/media/vOxiC0rt7hbs4/giphy.gif",
     alt: "Maverick singing in Top Gun GIF",
   },
 
   sing2: {
-    path: sing2,
     url: "https://media.giphy.com/media/EuYk9AYemhrDk8lh37/giphy-downsized-large.gif",
     alt: "Animated Viking women singing GIF",
   },
 
   dance1: {
-    path: dance1,
     url: "https://media.giphy.com/media/ygx2z9nIBYSeFPqt75/giphy.gif",
     alt: "Jimmy Fallon Dancing GIF",
   },
 
   dance2: {
-    path: dance2,
     url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXRhbDBvZnRoNWg4ZXZtdXVtdWVwanNuZ2tueGp5c3RvZnI0OW9yMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VcizxCUIgaKpa/giphy.gif",
     alt: "Wizard Dancing GIF",
   },
 
   reading1: {
-    path: reading1,
     url: "https://media.giphy.com/media/g2lH60ZcvUGL6/giphy.gif",
     alt: "Beast from Beauty and the Beast reading a book GIF",
   },
 
   talent1: {
-    path: talent1,
     url: "https://media.giphy.com/media/fQukKqcloXQn9l4wr9/giphy.gif",
     alt: "Bugs Bunny walking and jumping on the Piano GIF",
   },
 
   laugh1: {
-    path: laugh1,
     url: "https://media.giphy.com/media/116WjShb7DL3vq/giphy.gif",
     alt: "Mr. Bean showing Tongue GIF",
   },
 
   laugh2: {
-    path: laugh2,
     url: "https://media.giphy.com/media/DcTZZ1FAqXdfy/giphy.gif",
     alt: "Mr. Bean goofing around GIF",
   },
 
   eat1: {
-    path: eat1,
     url: "https://media.giphy.com/media/iZjoLdS1nwMRq/giphy.gif",
     alt: "Patrick eating food GIF",
   },
 
   robot1: {
-    path: robot1,
     url: "https://media.giphy.com/media/tczJoRU7XwBS8/giphy.gif",
     alt: "Robot dancing GIF",
   },
 
   robot2: {
-    path: robot2,
     url: "https://media.giphy.com/media/pIMlKqgdZgvo4/giphy.gif",
     alt: "Robot Army walking GIF",
   },
 
   propose1: {
-    path: propose1,
     url: "https://media.giphy.com/media/1wmOyZYoGzz003R03Y/giphy.gif",
     alt: "Tom from Tom and Jerry Proposing GIF",
   },
--- src/helpers/getGif.ts
+++ src/helpers/getGif.ts
@@ -1,4 +1,4 @@
-type gif = {
+export type gif = {
   path?: string;
   url: string;
   alt?: string;
